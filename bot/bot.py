30b2b5f867605ec8a4e375d2c5a5e7c1c35a2c93
shavel0k <anna.reznikova@icloud.com>
2025-10-21 16:25:41 +0300

    changed bot logic

--- bot/bot.py
+++ bot/bot.py
@@ -5,15 +5,16 @@ import telebot
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-bot = telebot.TeleBot(BOT_TOKEN)  # ← добавлено!
+bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    web_app = telebot.types.WebAppInfo(url="https://habr.com/ru/articles/886864/")
+    web_app = telebot.types.WebAppInfo(url="https://poly-esoteric-bot.vercel.app/")
     btn = telebot.types.KeyboardButton(text="Открыть мини-приложение", web_app=web_app)
     markup.add(btn)
     bot.send_message(message.chat.id, "Привет! Нажми кнопку ниже", reply_markup=markup)
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    print("Бот запущен...")
+    bot.infinity_polling()

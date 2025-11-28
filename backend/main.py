ffd46c1c8a238e1f264c0193f2ffcd5ea9411020
Матвей Таскаев <matveitaskaev@MacBook-Air-Matvej.local>
2025-11-29 03:04:05 +0400

    PPPP

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import horoscope, card
+from dotenv import load_dotenv
+from supabase import create_client
+import os
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise RuntimeError("❌ SUPABASE_URL или SUPABASE_KEY не найдены в .env")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI(title="PolyEsoteric Backend")
+
+@app.get("/test_db")
+def test_db():
+    try:
+        res = supabase.table("test_table").select("*").execute()
+        return {"status": "connected", "data": res.data}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,6 +31,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from routes import horoscope, card
 app.include_router(horoscope.router, prefix="/api")
 app.include_router(card.router, prefix="/api")
 

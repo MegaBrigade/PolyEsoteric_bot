1f9e6db75013c984ab4663db51a5fb836fa05a41
oksid-margantsa <galchalencha3@gmail.com>
2026-01-02 00:17:00 +0300

    fixing script for tarot page

--- frontend/js/tarot.js
+++ frontend/js/tarot.js
@@ -3,12 +3,6 @@ const mainCard = document.querySelector(".main-card");
 const cardsContainer = document.querySelector(".cards");
 const title = document.getElementById("title");
 
-const resultScreen = document.querySelector(".result-screen");
-const cardName = document.getElementById("cardName");
-const cardImage = document.getElementById("cardImage");
-const cardDescription = document.getElementById("cardDescription");
-const backButton = document.getElementById("backButton");
-
 let isOpen = false;
 
 returnButton.addEventListener("click", () => {
@@ -21,7 +15,7 @@ mainCard.addEventListener("click", async () => {
     title.textContent = "Готов узнать, что ждет тебя сегодня?";
     isOpen = true;
   } else {
-    await getDailyCard();
+    window.location.href = 'daily_card.html';
   }
 });
 
@@ -31,28 +25,3 @@ backButton.addEventListener("click", () => {
   title.textContent = "Нажми, чтобы открыть карту!";
   isOpen = false;
 });
-
-async function getDailyCard() {
-  try {
-    const response = await fetch('/api/card', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user_id: userId }),
-    });
-
-    if (!response.ok) throw new Error("Ошибка сервера");
-
-    const data = await response.json();
-
-    cardName.textContent = data.name;
-    cardDescription.textContent = data.meaning;
-    cardImage.src = "assets/card.svg";
-
-    resultScreen.classList.remove("hidden");
-  } catch (error) {
-    console.error(error);
-    title.textContent = "У карт нет совета для тебя сегодня!";
-  }
-}
